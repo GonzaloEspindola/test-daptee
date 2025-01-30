b76a55fa44abbe9cd1613e7ba6eea0c596a48100
Gonzalo Espindola <espindolamgonzalo@gmail.com>
2025-01-30 08:42:07 -0300

    initial nuxt app

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useAuthStore } from '~/store/auth'
+
+const { checkAuth } = useAuthStore()
+
+checkAuth()
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
