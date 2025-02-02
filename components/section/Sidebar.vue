87ea49b896c8d35ae5df575bb279a70f3d84eb68
Gonza Espindola <espindolamgonzalo@gmail.com>
2025-02-01 21:53:51 -0300

    login mobile fix style

--- components/section/Sidebar.vue
+++ components/section/Sidebar.vue
@@ -1,15 +1,15 @@
 <template>
-  <v-col cols="12" md="2" class="ma-0 pa-0 h-100">
+  <v-col cols="12" md="2" class="ma-0 pa-0">
     <v-list class="h-100 pa-3 bg-grey-darken-4">
-      <div :style="{ height: '10%' }">
-        <v-list-item class="border-b-sm h-100">
+      <div class="py-5">
+        <v-list-item class="h-100">
           <NuxtLink to="/">
             <v-img src="/assets/logo_daptee.svg" height="50" />
           </NuxtLink>
         </v-list-item>
       </div>
 
-      <div class="d-flex flex-column ga-2" :style="{ height: '90%' }">
+      <div class="d-flex flex-column ga-2 flex-grow-1">
         <v-list-item link to="/dashboard/users" class="rounded-lg">
           <v-list-item-content>
             <v-list-item-title>Usuarios</v-list-item-title>
